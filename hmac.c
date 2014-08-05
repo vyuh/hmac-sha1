1f6a88214e8391e17060b23bcd7c01dedc4a66d8
Prashant Karmakar <webster15july@gmail.com>
2014-08-05 14:53:39 +0530

    hmac_sha1() first complete draft :P

--- hmac.c
+++ hmac.c
@@ -1,8 +1,4 @@
-typedef struct {
-    unsigned char *d;
-    unsigned l[2];
-} bitstr;
-
+//TODO #include "sha1.h"
 #include <string.h>
 
 b8 *wr32be(b32 n, b8 *msg) {
@@ -13,26 +9,34 @@ b8 *wr32be(b32 n, b8 *msg) {
     *msg = n & mask;  return ++msg;
 } //libpk
 
-int hmac_sha1(bitstr *key, bitstr *msg, unsigned *code){
+int hmac_sha1(bitstr *key, bitstr *msg, unsigned mac[5]){
     unsigned k[16] = { 0 };
     unsigned o[16], i[16];
     unsigned x;
-    bitstr fin;
+    hsh h, fin;
 
 
-    if(key->[1] || (key->l[0] > 512)) {
-        //TODO int sha1(bitstr msg, b32 hash[5]);
-        //TODO rename the current sha1() to sha1_small
-        sha1(key->d, key->l, k); 
-        for(x=0; x<5; x++) wr32be(k[i], k+i); //FIXME: if wr32be works not
-    } else memcpy(k, key->d, key->l/8 + 1);
+    if(key->l[1]||key->l[0] > 512) {
+        sha1(key, &h); 
+        for(x=0; x<5; x++) wr32be(h.h[x], k+x); //FIXME: if wr32be works not
+    } else memcpy(k, key->d, key->l[0]/8 + (key->l[0]%8 && 1));
 
     for(x=0;x<16; x++) {
         o[x]=0x5c5c5c5c ^ k[x];
         i[x]=0x36363636 ^ k[x];
     }
 
-    fin.l[1]=msg->l[1];
-    fin.l[0]=msg->l[0]+ //TODO a good way to handle large msgs 
-    fin.d = malloc(
     
+    hsh_rst(&h);
+    sha1_nxt(i, 512, &h);
+    sha1_finish(msg, &h);
+    for(x=0; x<5; x++) wr32be(h.h[x], h.h+x); //FIXME: if wr32be works not
+    hsh_rst(&fin);
+    sha1_nxt(o, 512, &fin);
+    sha1_end(h.h, 160, &fin);
+ 
+    for(x=0; x<5; x++) mac[x]=fin.h[x];
+    return 0;
+
+}
+        
