4f4c0c8d87fd81b57d02df6c6a263834c416be81
Prashant Karmakar <webster15july@gmail.com>
2018-04-22 10:52:33 +0530

    support specifying msg and key as args

--- hmac-sha1-test.c
+++ hmac-sha1-test.c
@@ -2,17 +2,27 @@
 int main (int argc, char **argv) {
   b32 hash[5];
   char *msg = "hi";
+  char *key = "hi";
   bitstr m;
+  bitstr k;
 
   if (argc>1) {
     msg = argv[1];
+    key = argv[1];
+  }
+  if (argc>2) {
+    key = argv[2];
   }
 
   m.d = msg;
   m.l[0] = 8*strlen(msg);
   m.l[1] = 0;
 
-  hmac_sha1 (&m, &m, hash);
+  k.d = key;
+  k.l[0] = 8*strlen(key);
+  k.l[1] = 0;
+
+  hmac_sha1 (&k, &m, hash);
   rpr (hash);
   return 0;
 }
